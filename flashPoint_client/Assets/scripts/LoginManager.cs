6a4f389891f780fae59fc2adcf7d655bcf7d1761
Eason <tong.yang@mail.mcgill.ca>
2019-02-20 22:44:49 -0500

    .

--- flashPoint_client/Assets/scripts/LoginManager.cs
+++ flashPoint_client/Assets/scripts/LoginManager.cs
@@ -4,16 +4,19 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using SocketIO;
 using System;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour {
 
 	public SocketIOComponent socket;
 
+	public Text username;
 
 	void Start ()
 	{
 		Debug.Log(socket);
 		StartCoroutine(ConnectToServer());
+		socket.On("USER_CONNECTED", OnUserConnected );
 
 	}
 
@@ -22,11 +25,30 @@ public class LoginManager : MonoBehaviour {
 		yield return new WaitForSeconds(0.5f);
 
 		socket.Emit("USER_CONNECT");
+		
+		yield return new WaitForSeconds(0.5f);
+		
+		Dictionary<string, string> data = new Dictionary<string,string>();
+		//data['name'] = 'eason';
+		
+		//socket.Emit("PLAY");
 	}
 
-	void Update()
+	void OnUserConnected (SocketIOEvent obj)
 	{
-		
+		Debug.Log( "all user born on this client" );
+	}
+
+	public void LoginClick()
+	{
+		Debug.Log("login button clicked");
+		Debug.Log(username.text);
+	}
+
+	public void SignUpClick()
+	{
+		Debug.Log("Sign up button clicked");
+		Debug.Log(username.text);
 	}
 
 }
