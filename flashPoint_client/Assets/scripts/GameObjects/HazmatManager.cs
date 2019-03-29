b59082e68cf6bf356e7278995e92a4d04d39a7f0
sherry-zkuai <zirui.kuai@mail.mcgill.ca>
2019-03-28 20:03:05 -0400

    Add Hazmat

---
+++ flashPoint_client/Assets/scripts/GameObjects/HazmatManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazmatManager{
+    public GameManager gameManager;
+
+    public Dictionary<int[], GameObject> hdoorStores = new Dictionary<int[], GameObject>();
+}
