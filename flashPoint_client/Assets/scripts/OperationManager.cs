8821b3b0fd5c4835691d735c5314fa8fa4de2f1b
sherry-zkuai <zirui.kuai@mail.mcgill.ca>
2019-03-31 04:35:25 -0400

    Add New Vehicle UI, Fix Action

---
+++ flashPoint_client/Assets/scripts/OperationManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ActionManager
+{
+    public GameManager gm;
+
+    public Dictionary<ActionType, GameObject> prefabs= new Dictionary<ActionType,GameObject>();
+}
+
+public class Operation
+{
+
+}
+
+public enum ActionType
+{
+    Move,
+    ExtingSmoke,
+    ExtingFire,
+    Treat,
+    CarryV,
+    RemoveHazmat,
+    CarryHazmat,
+    LeadV,
+    Command,
+    Imaging,
+    Drive,
+    Remote,
+    Ride,
+
+}
